34b6aa4a6f69f5b5f1baa075083e3a9421162891
Maryam-n98 <mariam.mohannad98@gmail.com>
2021-08-10 04:27:48 +0300

    Finished lab 26

---
+++ app/src/main/java/com/example/taskmaster/AddTaskActivity.java
@@ -0,0 +1,29 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+        getActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button addNewTask =findViewById(R.id.button2);
+        addNewTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast buttonToast= Toast.makeText(AddTaskActivity.this,"Task Added!",Toast.LENGTH_SHORT);
+                buttonToast.show();
+            }
+        });
+    }
+
+}
